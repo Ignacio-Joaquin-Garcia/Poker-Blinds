2a80f107a6cf68475ad5a28bc3ba9ef7c0574563
ignaciogarcia <garciaignaciouni@gmail.com>
2025-07-04 17:43:57 -0300

    App Totalmente Funcional, Faltan apartados visuales

--- js/index.js
+++ js/index.js
@@ -1,12 +1,31 @@
 //Setting por defecto si no estan configurados
 let settings = {roundMinutes: 10};
+let arrayRounds = [];
+let index = 0;
+const audio = new Audio('../assets/audio/acdc.m4a');
 //Carga de Datos Fundamentales
 function loadDataFromSettings(){
+    //Temporizador
     let roundMinutesAnterioresJSON = localStorage.getItem("roundMinutes");
     let roundMinutesAnteriores = JSON.parse(roundMinutesAnterioresJSON);
     console.log(roundMinutesAnteriores)
     if(roundMinutesAnteriores != null){
         settings.roundMinutes = roundMinutesAnteriores.roundMinutes;
+    }
+    
+    //Carga de datos SI EXISTEN de BLINDS
+    let arrayRoundsJson = localStorage.getItem("arrayRounds");
+    if (arrayRoundsJson && arrayRoundsJson !== "[]") {
+        let datosAnteriorArrayRounds = JSON.parse(arrayRoundsJson);
+        console.log(datosAnteriorArrayRounds);
+        arrayRounds = datosAnteriorArrayRounds;
+    } else {
+        arrayRounds.push({
+    round: 1,
+    minBlind: 1,
+    maxBlind: 2
+})
+        console.log(arrayRounds)
     }
 }
 document.addEventListener("DOMContentLoaded", ()=>{
@@ -25,7 +44,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
     botonEmpezarTemportizador.addEventListener("click", temporizadorControl);
     pTemporizador.addEventListener("click", temporizadorControl);
     function temporizadorControl(){
-        if (pausarTemporizador){
+        
+        if (cronometroMinutos === 0 && cronometroSegundos === 0 && botonEmpezarTemportizador.style.display === "none"){
+            cronometroMinutos = tiempoInicial-1;
+            cronometroSegundos = 59;
+            pausarTemporizador = false;
+            empezarTemporizador();
+        }else if(pausarTemporizador){
             pausarTemporizador = false;
             botonEmpezarTemportizador.style.display = "none";
             pTemporizador.style.display = "block";
@@ -37,10 +62,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
             pausarTemporizador = true;
         }
     }
+    
     for(let i = 0; i<2;i++){
         botonesResetTemportizador[i].addEventListener("click", resetTemporizador);
-        //BLINDS CHANGE
-
     }
 
     function actualizarTextoP(){
@@ -57,7 +81,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
             }
             if ((cronometroMinutos === 0) && (cronometroSegundos === 0)){
                 pausarTemporizador = true;
+                
                 clearInterval(displayTemporizador);
+                audio.play()
+                setTimeout(()=>{audio.pause()},7500);
             }
             actualizarTextoP()
         },1000)
@@ -71,6 +98,28 @@ document.addEventListener("DOMContentLoaded", ()=>{
         cronometroSegundos = 59;
         actualizarTextoP();
     }
+
+    const pMinBlind = document.getElementById("apuestaMinima");
+    const pMaxBlind = document.getElementById("apuestaMaxima");
+
+    function mostrarBlind(math){
+        index = index + math;
+        console.log(arrayRounds.length)
+        if (index >= arrayRounds.length){
+            index--;
+            resetTemporizador();
+        } else if(index < 0){
+            index++;
+            resetTemporizador();
+        } else{
+            console.log(arrayRounds[index].minBlind);
+            pMinBlind.textContent = arrayRounds[index].minBlind;
+            pMaxBlind.textContent = arrayRounds[index].maxBlind;
+        }
+    }
+    mostrarBlind(0);
+    botonesResetTemportizador[0].addEventListener("click", ()=>{mostrarBlind(-1)});
+    botonesResetTemportizador[1].addEventListener("click",()=>{mostrarBlind(1)});
 });
 
 
